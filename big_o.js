4b0c1a365435e3b811c9df1e0a59f33ca016bf10
kenderb <kenderboliar@gmail.com>
2020-11-09 12:09:17 -0500

    Add O(logUnder2n) Logarithmic complexity

--- big_o.js
+++ big_o.js
@@ -34,3 +34,14 @@ function exampleLinear(n) {
     console.log(i);
   }
 }
+
+/* O(log2n)
+Logarithmic complexity. For a given n, this will operate only log2n
+times because i is incremented by multiplying by 2 rather than
+adding 1 as in the other examples. */
+
+function someFunction(n) {
+  for (var i = 0; i < n; i * 2) {
+  console.log(n);
+  }
+}
