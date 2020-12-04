0128aac6964e7942737c73156ba72d81c17ac4df
kenderb <kenderbolivar@gmail.com>
2020-12-04 15:21:05 -0500

    Refacture anagram challenge

--- anagram.js
+++ anagram.js
@@ -1,17 +1,19 @@
 function anagram(s) {
   if (s.length % 2 != 0) return -1;
-
-  var middle = Math.ceil(s.length / 2);
-  var s1 = s.slice(0, middle);
-  var s2 = s.slice(middle);
-
-  s1.split('').forEach(n => {
-    if (s2.includes(n)) {
-      s1 = s1.replace(n, '');
-      s2 = s2.replace(n, '');
+  spplitArray = split_in_half(s);
+  spplitArray[0].split('').forEach(n => {
+    if (spplitArray[1].includes(n)) {
+      spplitArray[0] = spplitArray[0].replace(n, '');
+      spplitArray[1] = spplitArray[1].replace(n, '');
     }
   });
-  return (s1.length + s2.length)/2;
+  return (spplitArray[0].length + spplitArray[1].length)/2;
 }
 
-console.log(anagram('dfgas'));
+function split_in_half(s) {
+  var middle = Math.ceil(s.length / 2);
+  return [s.slice(0, middle), s.slice(middle)];
+  
+}
+
+console.log(anagram('dfgasf'));
