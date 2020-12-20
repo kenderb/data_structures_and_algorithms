fe2c9e20252b2dc1fca02cc8adf7c852bae2a1e4
kenderb <kenderbolivar@gmail.com>
2020-12-20 18:50:46 -0500

    Add solution for anagram O(n])

--- anagram.js
+++ anagram.js
@@ -1,19 +1,36 @@
+
+//n(O) anagram solution
 function anagram(s) {
   if (s.length % 2 != 0) return -1;
-  spplitArray = split_in_half(s);
-  spplitArray[0].split('').forEach(n => {
-    if (spplitArray[1].includes(n)) {
-      spplitArray[0] = spplitArray[0].replace(n, '');
-      spplitArray[1] = spplitArray[1].replace(n, '');
+  const spplitArray = split_in_half(s);
+  const string1 = spplitArray[0];
+  const string2 = spplitArray[1];
+  var obj2 = counter(string2);
+  var sum = 0
+  for (let letter of string1) {
+    if (letter in obj2) {
+      obj2[letter] -= 1;
     }
-  });
-  return (spplitArray[0].length + spplitArray[1].length)/2;
+  }
+  for (const key in obj2) {
+    if (obj2[key]>0) {
+      sum += obj2[key];
+    }
+  }
+  return sum;
 }
 
 function split_in_half(s) {
   var middle = Math.ceil(s.length / 2);
   return [s.slice(0, middle), s.slice(middle)];
-  
 }
 
-console.log(anagram('dfgasf'));
+function counter(string) {
+  let frequencyCounter1= {};
+  for (let letter of string) {
+    frequencyCounter1[letter] = (frequencyCounter1[letter] || 0) + 1;
+  }
+  return frequencyCounter1;
+}
+
+console.log(anagram('fdhlvosfpafhalll'));
