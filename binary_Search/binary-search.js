17502333fe1011b9a9dcb6ce894b8acdaa2e7560
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-02 20:21:24 -0500

    Add a fast return

--- binary_Search/binary-search.js
+++ binary_Search/binary-search.js
@@ -1,6 +1,7 @@
-function binarySearch(array, num) {
+const binarySearch = (array, num) => {
   let min = 0;
   let max = array.length - 1;
+  if (num > array[array.length - 1]) return -1;
   while (min < array[array.length - 1]) {
     const avarage = Math.floor((min + max) / 2);
     if (num === array[avarage]) {
@@ -13,7 +14,7 @@ function binarySearch(array, num) {
     }
   }
   return -1;
-}
+};
 
 
 console.log(binarySearch([1, 2, 3, 4, 5, 6], 2));
@@ -21,4 +22,4 @@ console.log(binarySearch([1, 2, 3, 4, 5, 6], 2));
 console.log(binarySearch([1, 2, 3, 4, 5], 3));
 console.log(binarySearch([1, 2, 3, 4, 5], 5));
 console.log(binarySearch([1, 2, 3, 4, 5], 6));
-console.log(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10));
+console.log(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 330));
