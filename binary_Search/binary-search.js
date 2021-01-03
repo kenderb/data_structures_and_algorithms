246bb454cf0a685843af8e120ef5ac716f920860
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-02 20:15:20 -0500

    Add binarySearch solution

--- binary_Search/binary-search.js
+++ binary_Search/binary-search.js
@@ -1,9 +1,24 @@
-const binarySearch = (array, num) => {
-  const arrayLength = array.length;
-  if (num > arrayLength) return -1;
-};
+function binarySearch(array, num) {
+  let min = 0;
+  let max = array.length - 1;
+  while (min < array[array.length - 1]) {
+    const avarage = Math.floor((min + max) / 2);
+    if (num === array[avarage]) {
+      return avarage;
+    }
+    if (array[avarage] < num) {
+      min += 1;
+    } else if (array[avarage] > num) {
+      max -= 1;
+    }
+  }
+  return -1;
+}
 
 
-console.log(binarySearch([1, 2, 3, 4, 5], 2));
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 2));
+//
 console.log(binarySearch([1, 2, 3, 4, 5], 3));
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
 console.log(binarySearch([1, 2, 3, 4, 5], 6));
+console.log(binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10));
