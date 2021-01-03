b872ced9b35d8a044dbcc0763a2fe966786e75ad
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-03 10:51:38 -0500

    Fix linter errors for bubble sort

--- bubble_sort/Bubble_sort_optimization.js
+++ bubble_sort/Bubble_sort_optimization.js
@@ -1,19 +1,19 @@
 // Optimized BubbleSort with noSwaps
-function bubbleSort(arr){
-  var noSwaps;
-  for(let i = arr.length; i > 0; i--){
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i -= 1) {
     noSwaps = true;
-    for(let j = 0; j < i - 1; j++){
-      if(arr[j] > arr[j+1]){
-        var temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
-        noSwaps = false;         
+    for (let j = 0; j < i - 1; j += 1) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
-    if(noSwaps) break;
+    if (noSwaps) break;
   }
   return arr;
 }
 
-bubbleSort([8,1,2,3,4,5,6,7]);
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
