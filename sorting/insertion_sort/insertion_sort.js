568324194025db79fa53a38605ac39fd558c55f0
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-05 08:06:49 -0500

    Refactor insertion sort

--- sorting/insertion_sort/insertion_sort.js
+++ sorting/insertion_sort/insertion_sort.js
@@ -1,13 +1,14 @@
-function insertionSort(arr){
-	var currentVal;
-    for(var i = 1; i < arr.length; i++){
-        currentVal = arr[i];
-        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-            arr[j+1] = arr[j]
-        }
-        arr[j+1] = currentVal;
+function insertionSort(arr) {
+  let j;
+  let currentVal;
+  for (let i = 1; i < arr.length; i += 1) {
+    currentVal = arr[i];
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j -= 1) {
+      arr[j + 1] = arr[j];
     }
-    return arr;
+    arr[j + 1] = currentVal;
+  }
+  return arr;
 }
 
-insertionSort([2,1,9,76,4])
+insertionSort([2, 1, 9, 76, 4]);
