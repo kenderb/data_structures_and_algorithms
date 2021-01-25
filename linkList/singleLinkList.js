34f68c387bd8c6fc22eff01edd79ff7c06f83243
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-25 09:43:03 -0500

    Add th singlyLinkList class

---
+++ linkList/singleLinkList.js
@@ -0,0 +1,12 @@
+import Node from './node';
+
+export default class SinglyLinkList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val){
+    
+  }
+}
