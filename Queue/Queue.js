d8086847c2f8e0dcf2976f62694fc9bbdfad714a
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-28 11:03:05 -0500

    Add Queue class and tests for push method

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -1,1 +1,22 @@
 import Node from '../linkList/node';
+
+export default class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = this.first;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size += 1;
+    return this.size;
+  }
+}
