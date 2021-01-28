d0a0ccfe27beb9615d4db678dd44e97251fc1a70
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-28 11:16:08 -0500

    Add shift method the Queue class and its tests

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -7,16 +7,28 @@ export default class Queue {
     this.size = 0;
   }
 
-  push(value) {
-    const newNode = new Node(value);
-    if (!this.first) {
+  push(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
       this.first = newNode;
       this.last = this.first;
     } else {
-      newNode.next = this.first;
-      this.first = newNode;
+      this.last.next = newNode;
+      this.last = newNode;
     }
     this.size += 1;
     return this.size;
+  }
+
+  shift() {
+    if (!this.first) return null;
+    const currentHead = this.first;
+    this.first = currentHead.next;
+    this.size -= 1;
+    if (this.size === 0) {
+      this.first = null;
+      this.last = null;
+    }
+    return currentHead.val;
   }
 }
--- __tests__/Queue.test.js
+++ __tests__/Queue.test.js
@@ -14,4 +14,25 @@ describe('Queue class', () => {
     newStack.push(value);
     expect(newStack.size).toEqual(1);
   });
+
+  it('should shift a value in the Queue', () => {
+    const newStack = new Queue();
+    const value = 'test';
+    newStack.push(value);
+    newStack.push('value');
+    newStack.push('one');
+    newStack.push('last');
+    expect(newStack.shift()).toEqual('test');
+  });
+
+  it('should shift a value in the Queue and decrese the size', () => {
+    const newStack = new Queue();
+    newStack.push('value');
+    newStack.push('one');
+    newStack.push('last');
+    newStack.shift();
+    newStack.shift();
+    newStack.shift();
+    expect(newStack.shift()).toEqual(null);
+  });
 });
