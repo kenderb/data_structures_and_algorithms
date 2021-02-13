6bfcc15f6aefb056191389632a8faedf4124e568
kenderb <kenderbolivar@gmail.com>
2021-02-13 14:52:36 -0500

    Add 136. Single Number to the repo

---
+++ 136_single_number/single.js
@@ -0,0 +1,5 @@
+var singleNumber = function(nums) {
+    
+};
+
+export default singleNumber;
---
+++ 136_single_number/single.test.js
@@ -0,0 +1,7 @@
+import singleNumber from './single';
+
+describe('contains Duplicate function', () => {
+  it('should be a function', () => {
+    expect(typeof singleNumber).toBe('function');
+  });
+});
