f43a013687d14ec64d19da7855af6f26b4c7c15e
kenderb <kenderbolivar@gmail.com>
2021-02-13 12:07:28 -0500

    Add best Runtime solution to the missing numbers

--- 268_missing_number/missing.js
+++ 268_missing_number/missing.js
@@ -1,14 +1,24 @@
+
 var missingNumber = function(nums) {
-  const numsHas = {};
-  nums.forEach((element, index)=> {
-    numsHas[element] = index;
-  });
-  if (numsHas[0] === undefined) return 0;
-  console.log(numsHas);
-  for (let i = 0; i < nums.length; i++) {
-    const nextNum = i + 1
-    if (numsHas[nextNum] === undefined) return nextNum;
-  }
-};
+  const missingNumsTotal = nums.reduce((total, num) => total + num)
+  let fullTotal = 0;
+  for (let i = 0; i <= nums.length; i++) fullTotal += i;
+  return (fullTotal - missingNumsTotal);
+}
+
+
+
+// var missingNumber = function(nums) {
+//   const numsHas = {};
+//   nums.forEach((element, index)=> {
+//     numsHas[element] = index;
+//   });
+//   if (numsHas[0] === undefined) return 0;
+//   console.log(numsHas);
+//   for (let i = 0; i < nums.length; i++) {
+//     const nextNum = i + 1
+//     if (numsHas[nextNum] === undefined) return nextNum;
+//   }
+// };
 
 export default missingNumber;
