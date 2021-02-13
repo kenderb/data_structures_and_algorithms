ea6074a7c7a98edf5ab6c1a638376bfbe336cb27
kenderb <kenderbolivar@gmail.com>
2021-02-13 13:43:50 -0500

    remove some spaces

--- 448_find_all_numbers_disappeared/find.js
+++ 448_find_all_numbers_disappeared/find.js
@@ -2,9 +2,7 @@ var findDisappearedNumbers = function(nums) {
   const numsHas = new Set;
   let findArray = []
   nums.forEach((num) => numsHas.add(num));
-  for (let i = 1; i <= nums.length; i++) {
-    if(!numsHas.has(i)) findArray.push(i);
-  }
+  for (let i = 1; i <= nums.length; i++) if(!numsHas.has(i)) findArray.push(i);
   return findArray;
 };
 export default findDisappearedNumbers;
