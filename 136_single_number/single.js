1401bf618e5a3d2b89088b56b3139f31c5df80e7
kenderb <kenderbolivar@gmail.com>
2021-02-13 15:24:37 -0500

    Add a solution for 136. Single Number Runtime 85 ms

--- 136_single_number/single.js
+++ 136_single_number/single.js
@@ -1,5 +1,13 @@
 var singleNumber = function(nums) {
-    
+  const newHahs = {};
+  nums.forEach(num => {
+    if (newHahs[num]) delete(newHahs[num]);
+    else newHahs[num] = 1;
+  });
+  for (const key in newHahs) {
+    return Number(key);
+  }
+  return false;
 };
 
 export default singleNumber;
--- 136_single_number/single.test.js
+++ 136_single_number/single.test.js
@@ -4,4 +4,13 @@ describe('contains Duplicate function', () => {
   it('should be a function', () => {
     expect(typeof singleNumber).toBe('function');
   });
+  it('should return 1 given the array [2,2,1]', () => {
+    expect(singleNumber([2,2,1])).toBe(1);
+  });
+  it('should return 4 given the array [4,1,2,1,2]', () => {
+    expect(singleNumber([4,1,2,1,2])).toBe(4);
+  });
+  it('should return 1 given the array [1]', () => {
+    expect(singleNumber([1])).toBe(1);
+  });
 });
