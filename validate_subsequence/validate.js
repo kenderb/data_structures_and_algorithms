6221a01fd55bfcb1e1ba18859e17cceef5cd0c99
kenderb <kenderbolivar@gmail.com>
2021-02-15 21:50:31 -0500

    Add solution for validate usgin for loop

--- validate_subsequence/validate.js
+++ validate_subsequence/validate.js
@@ -1,18 +1,21 @@
 const validate = (array, sequence) => {
   const arrayHash = {};
-  let arrayIndex = []
+  let arrayIndex = [] 
 
   // fill the object
-  array.forEach((num, index) => arrayHash[num] = index);
+  array.forEach((num, index) => arrayHash[num] = index); // O(n)
 
   // filter the index of the array
-  sequence.forEach( num => {if (arrayHash[num] !== undefined ) arrayIndex.push(arrayHash[num])});
+  sequence.forEach( num => {if (arrayHash[num] !== undefined ) arrayIndex.push(arrayHash[num])}); // O(n)
 
   // edge case for the array length
   if (arrayIndex.length !== sequence.length) return false;
   
-  // return if the index are in secuence or not
-  return arrayIndex.every((num, index) => (index === arrayIndex.length - 1 || num < arrayIndex[index + 1]))
+  // return false if the arrayIndex ind not sequencial
+  for (let i = 0; i < arrayIndex.length - 1; i ++) { // O(n)
+    if (!(arrayIndex[i] < arrayIndex[i + 1])) return false
+  }
+  return true;
 }
 
 export default validate;
