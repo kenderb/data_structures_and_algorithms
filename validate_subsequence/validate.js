df95b5792b2cda440dfc158a6e623e8f49602b8a
kenderb <kenderbolivar@gmail.com>
2021-02-15 20:08:10 -0500

    Add a solution for validate

--- validate_subsequence/validate.js
+++ validate_subsequence/validate.js
@@ -1,5 +1,19 @@
 const validate = (array, sequence) => {
-  return true;
+  // Crate empty array and an object
+  const arrayHash = {};
+  let arrayIndex = []
+
+  // fill the object
+  array.forEach((num, index) => arrayHash[num] = index);
+
+  // filter the index of the array
+  sequence.forEach( num => {if (arrayHash[num] !== undefined ) arrayIndex.push(arrayHash[num])});
+
+  // edge case for the array length
+  if (arrayIndex.length !== sequence.length) return false;
+  return arrayIndex.every((num, index) => {
+    return index === arrayIndex.length - 1 || num < arrayIndex[index + 1];
+  })
 }
 
 export default validate;
--- validate_subsequence/validate.test.js
+++ validate_subsequence/validate.test.js
@@ -4,13 +4,22 @@ describe('contains Duplicate function', () => {
   it('should be a function', () => {
     expect(typeof validate).toBe('function');
   });
-  it('should return true giving this array [5, 1, 22, 25, 6, -1, 8, 10] with sequence = [1, 6, -1, 10]', () => {
+  it('should return TRUE giving this array [1, 2, 3, 4] with sequence = [1, 3, 4]', () => {
+    expect(validate([1, 2, 3, 4], [1, 3, 4])).toBe(true);
+  });
+  it('should return FALSE giving this array [2, 1, 4, 3] with sequence = [1, 3, 4]', () => {
+    expect(validate([2, 1, 4, 3], [1, 3, 4])).toBe(false);
+  });
+  it('should return TRUE giving this array [1, 2, 3, 4] with sequence = [2, 4]', () => {
+    expect(validate([1, 2, 3, 4], [2, 4])).toBe(true);
+  });
+  it('should return FALSE giving this array [5, 1, 22, 25, 6, -1, 8, 10] with sequence = [1, 6, -1, 10]', () => {
     expect(validate([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])).toBe(true);
   });
-  it('should return true giving this array [5, 1, 2, 25, 6, 3, 4, 10, 6] with sequence = [2, 3, 4, 6]', () => {
+  it('should return TRUE giving this array [5, 1, 2, 25, 6, 3, 4, 10, 6] with sequence = [2, 3, 4, 6]', () => {
     expect(validate([5, 1, 2, 25, 6, 3, 4, 10, 6], [2, 3, 4, 6])).toBe(true);
   });
-  it('should return true giving this array [5, 1, 2] with sequence = [2, 3, 4, 6]', () => {
+  it('should return FALSE giving this array [5, 1, 2] with sequence = [2, 3, 4, 6]', () => {
     expect(validate([5, 1, 2], [2, 3, 4, 6])).toBe(false);
   });
 });
