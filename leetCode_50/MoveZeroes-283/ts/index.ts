5f341b5357c1537652fa62006baa1e2908851f6a
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-07 12:47:59 -0500

    Add the test for the TypeScript soplution

---
+++ leetCode_50/MoveZeroes-283/ts/index.ts
@@ -0,0 +1,3 @@
+export const moveZeros = (arr: number[]): number[] => {
+  return [];
+};
---
+++ leetCode_50/MoveZeroes-283/ts/solution.test.ts
@@ -0,0 +1,6 @@
+import {moveZeros} from './index';
+describe('Describe moveZeros function', () => {
+  it('Should be a function', () => {
+    expect(typeof moveZeros).toBe('function');
+  })
+});
