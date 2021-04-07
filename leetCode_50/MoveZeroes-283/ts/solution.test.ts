971b82783b8967ead36049c714f1999e73a5abf6
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-07 12:54:44 -0500

    Add more test for the MoveZeroes challenge

--- leetCode_50/MoveZeroes-283/ts/solution.test.ts
+++ leetCode_50/MoveZeroes-283/ts/solution.test.ts
@@ -3,4 +3,19 @@ describe('Describe moveZeros function', () => {
   it('Should be a function', () => {
     expect(typeof moveZeros).toBe('function');
   })
+  it('Should return the right array, input: [0, 1, 3, 5, 0], output: [1, 3, 5, 0, 0]', () => {
+    const input = [0, 1, 3, 5, 0];
+    const result = [1, 3, 5, 0, 0];
+    expect(moveZeros(input)).toBe(result);
+  })
+  it('Should return the right array, input: [0], output: [0]', () => {
+    const input = [0];
+    const result = [0];
+    expect(moveZeros(input)).toBe(result);
+  })
+  it('Should return the right array, input: [], output: []', () => {
+    const input = [];
+    const result = [];
+    expect(moveZeros(input)).toStrictEqual(result);
+  })
 });
