77b68e0cfea1b013dded2abd92653d16a0b00ecb
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-07 13:51:52 -0500

    Add Ruby tests for MoveZeroes

--- leetCode_50/MoveZeroes-283/ruby/spec/test_spec.rb
+++ leetCode_50/MoveZeroes-283/ruby/spec/test_spec.rb
@@ -1,13 +1,36 @@
 require 'move_zeros'
 
   describe "moveZeros" do
+  
     
-    context "a context" do
-      it "Returns the right array, input: [0, 1, 3, 5, 0], output: [1, 3, 5, 0, 0]" do
-        input = [0, 1, 3, 5, 0]
-        output =  [1, 3, 5, 0, 0]
-        expect(moveZeros(input)).to eq(output)
-      end
+    it "Returns the right array, input: [0, 1, 3, 5, 0], output: [1, 3, 5, 0, 0]" do
+      input = [0, 1, 3, 5, 0]
+      output =  [1, 3, 5, 0, 0]
+      expect(moveZeros(input)).to eq(output)
+    end
+    
+    it "Should return the right array, input: [0, 0, 3, 0, 32], output: [3, 32, 0, 0, 0]" do
+      input = [0, 0, 3, 0, 32]
+      output =  [3, 32, 0, 0, 0]
+      expect(moveZeros(input)).to eq(output)
+    end
+    
+    it "Should return the right array, input: [10, 2, 0, 8, 20], output: [10, 2, 8, 20, 0]" do
+      input = [0, 1, 3, 5, 0]
+      output =  [1, 3, 5, 0, 0]
+      expect(moveZeros(input)).to eq(output)
+    end
+    
+    it "Should return the right array, input: [0], output: [0]" do
+      input = [0]
+      output =  [0]
+      expect(moveZeros(input)).to eq(output)
+    end
+    
+    it "Should return the right array, input: [], output: []" do
+      input = []
+      output =  []
+      expect(moveZeros(input)).to eq(output)
     end
     
   end
