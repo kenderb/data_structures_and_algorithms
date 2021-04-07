f1b050e632933a106368538761c732c1068e6d2d
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-07 13:09:08 -0500

    Add TS solution

--- leetCode_50/MoveZeroes-283/ts/index.ts
+++ leetCode_50/MoveZeroes-283/ts/index.ts
@@ -1,4 +1,16 @@
 export const moveZeros = (arr: number[]): number[] => {
-  let zeroIndex = 0
+  let zeroIndex: number = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] !== 0) {
+      arr[zeroIndex] = arr[i];
+      zeroIndex += 1;
+    }
+  }
+  
+  for(let i = zeroIndex; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+  
   return arr;
 };
--- leetCode_50/MoveZeroes-283/ts/solution.test.ts
+++ leetCode_50/MoveZeroes-283/ts/solution.test.ts
@@ -9,6 +9,11 @@ describe('Describe moveZeros function', () => {
     const result = [1, 3, 5, 0, 0];
     expect(moveZeros(input)).toStrictEqual(result);
   });
+  it('Should return the right array, input: [0, 0, 3, 0, 32], output: [3, 32, 0, 0, 0]', () => {
+    const input = [0, 0, 3, 0, 32];
+    const result = [3, 32, 0, 0, 0];
+    expect(moveZeros(input)).toStrictEqual(result);
+  });
   
   it('Should return the right array, input: [10, 2, 0, 8, 20], output: [10, 2, 8, 20, 0]', () => {
     const input = [0, 1, 3, 5, 0];
