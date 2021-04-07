a31ea3d06be25c3b37d1373849b188b0f2b9e9cc
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-07 14:34:33 -0500

    Add a solution for the Ruby version of move zeros challenge

--- leetCode_50/MoveZeroes-283/ruby/lib/move_zeros.rb
+++ leetCode_50/MoveZeroes-283/ruby/lib/move_zeros.rb
@@ -1,3 +1,16 @@
 def moveZeros(arr)
+  zero_index = 0
+  
+  arr.each_with_index do |number, idx|
+    unless number.zero?
+      arr[zero_index] = number
+      zero_index += 1
+    end
+  end
+  
+  for i in zero_index..arr.length - 1 do 
+    arr[i] = 0 
+  end
+  
   arr
 end
