adb19ebd083a1c2fe8f82f02be8c96afdb1f688a
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-07 16:49:38 -0500

    Add a solution for numRescueBoats TypeScript

--- leetCode_50/Boats_to_Save_People-881/ts/index.ts
+++ leetCode_50/Boats_to_Save_People-881/ts/index.ts
@@ -1,4 +1,17 @@
 export const numRescueBoats = (people: number[], limit: number): number => {
-  let numbersOfBoats = 0;
+  let left: number = 0;
+  let right: number = people.length - 1;
+  let numbersOfBoats: number = 0;
+  
+  people = people.sort((a, b) => a - b);
+  
+  while (left <= right) {
+    
+    if(left === right) return numbersOfBoats += 1;
+    if(people[left] + people[right] <= limit) left += 1;
+    right -= 1;
+    numbersOfBoats += 1;
+  }
+
   return numbersOfBoats;
 };
--- leetCode_50/Boats_to_Save_People-881/ts/solution.test.ts
+++ leetCode_50/Boats_to_Save_People-881/ts/solution.test.ts
@@ -4,24 +4,31 @@ describe('Describe moveZeros function', () => {
     expect(typeof numRescueBoats).toBe('function');
   });
   
-  it('Should return the right result, Input: people = [1,2], limit = 3, Output: 1', () => {
+  it('Should return the right result, Input: people = [1,2], limit = 3, result: 1', () => {
     const people = [1,2];
     const limit = 3;
     const result = 1;
     expect(numRescueBoats(people, limit)).toBe(result);
   });
   
-  it('Should return the right result, Input: people = [3,2,2,1], limit = 3, Output: 3', () => {
+  it('Should return the right result, Input: people = [3,2,2,1], limit = 3, result: 3', () => {
     const people = [3,2,2,1];
     const limit = 3;
     const result = 3;
     expect(numRescueBoats(people, limit)).toBe(result);
   });
   
-  it('Should return the right result, Input: people = [3,5,3,4], limit = 5 Output: 4', () => {
+  it('Should return the right result, Input: people = [3,5,3,4], limit = 5 result: 4', () => {
     const people = [3,5,3,4];
     const limit = 5;
     const result = 4;
+    expect(numRescueBoats(people, limit)).toBe(result);
+  });
+  
+  it('Should return the right result, Input: people = [44,10,29,12,49,41,23,5,17,26], limit = 50, result: 6', () => {
+    const people = [44,10,29,12,49,41,23,5,17,26];
+    const limit = 50;
+    const result = 6;
     expect(numRescueBoats(people, limit)).toBe(result);
   });
   
