133911363a1b20e7b9832af4c9b7d4f7a615043d
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-08 08:25:34 -0500

    Add a solution for challenge 941 using ruby

--- leetCode_50/Valid_Mountain_Array-941/ruby/main.rb
+++ leetCode_50/Valid_Mountain_Array-941/ruby/main.rb
@@ -1,3 +1,16 @@
 def validMountainArray(arr)
-  false
+  current = 1
+  
+  while current < arr.length && arr[current] > arr[current - 1]
+    current += 1
+  end
+  
+  return false if current === 1 || current === arr.length
+  
+  while current < arr.length && arr[current] < arr[current - 1]
+    current += 1
+  end
+  
+  current === arr.length
+  
 end
