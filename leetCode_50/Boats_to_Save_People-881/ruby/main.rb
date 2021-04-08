e4f06d35f25f37cfc6193e8bed31df82b2bbb2e4
kenderb <40863532+kenderb@users.noreply.github.com>
2021-04-07 20:56:34 -0500

    Add a solution for the ruby version of numRescueBoats

--- leetCode_50/Boats_to_Save_People-881/ruby/main.rb
+++ leetCode_50/Boats_to_Save_People-881/ruby/main.rb
@@ -1,3 +1,15 @@
 def numRescueBoats(people, limit)
   boats_number = 0
+  left = 0
+  rigth = people.length - 1
+  people.sort!
+  
+  while left <= rigth
+    if people[left] + people[rigth] <= limit
+      left += 1
+    end
+    rigth -= 1
+    boats_number += 1
+  end
+  boats_number
 end
